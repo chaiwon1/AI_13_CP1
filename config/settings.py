5da7f36a3331162c7554663fbf3197bf01212332
현채원 <chaiwonhyun@hyeonchaewon-ui-MacBookAir.local>
2022-09-12 16:02:05 +0900

    done

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+# STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
